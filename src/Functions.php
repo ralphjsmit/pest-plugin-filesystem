bebccc4ec897061afeff4d2878be071f65945d32
Ralph J. Smit <59207045+ralphjsmit@users.noreply.github.com>
2021-12-17 19:14:15 +0100

    Add rmdir_recursive

--- src/Functions.php
+++ src/Functions.php
@@ -2,7 +2,18 @@
 
 namespace RalphJSmit\PestPluginFilesystem;
 
-function example()
+function rmdir_recursive(string $dir): void
 {
-    expect(true)->toBe(true);
+    foreach (scandir($dir) as $file) {
+        if ('.' === $file || '..' === $file) {
+            continue;
+        }
+
+        if (is_dir("$dir/$file")) {
+            rmdir_recursive("$dir/$file");
+        } else {
+            unlink("$dir/$file");
+        }
+    }
+    rmdir($dir);
 }
