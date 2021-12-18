8e0e212b3aeec22e64d63f5b2ea8964b5619924b
Ralph J. Smit <59207045+ralphjsmit@users.noreply.github.com>
2021-12-18 13:40:23 +0100

    Add test for expect(...)->toHaveContents() expectation

--- tests/ExpectationsTest.php
+++ tests/ExpectationsTest.php
@@ -1,6 +1,9 @@
 <?php
 
+use PHPUnit\Framework\AssertionFailedError;
+
 use function RalphJSmit\PestPluginFilesystem\expectFailedAssertion;
+use function RalphJSmit\PestPluginFilesystem\rm;
 
 beforeEach(function () {
     if ( file_exists(__DIR__ . '/test.php') ) {
@@ -27,4 +30,33 @@ it('it can correctly determine if a file does\'t exist', function () {
     expectFailedAssertion($this);
 
     expect(__DIR__ . '/test.php')->toExist();
+});
+
+test('it can correctly determine if a file has certain contents', function () {
+    rm(__DIR__ . '/tmp/fileA.php');
+    rm(__DIR__ . '/tmp/fileB.php');
+    rm(__DIR__ . '/tmp/fileC.php');
+
+    file_put_contents(__DIR__ . '/tmp/fileA.php', 'I\'m a test file!');
+    file_put_contents(__DIR__ . '/tmp/fileB.php', 'I\'m a second test file!');
+    file_put_contents(__DIR__ . '/tmp/fileC.php', 'I\'m an empty file!');
+
+    expect(__DIR__ . '/tmp/fileA.php')->toHaveContents('I\'m a test file!');
+    expect(__DIR__ . '/tmp/fileB.php')->toHaveContents('I\'m a second test file!');
+    expect(__DIR__ . '/tmp/fileC.php')->not->toHaveContents('I\'m a test file!');
+
+    expect(function () {
+        expect(__DIR__ . '/tmp/fileA.php')
+            ->toHaveContents('I\'m a regular file!');
+    })->toThrow(AssertionFailedError::class);
+
+    expect(function () {
+        expect(__DIR__ . '/tmp/fileB.php')
+            ->toHaveContents('I\'m a regular file!');
+    })->toThrow(AssertionFailedError::class);
+
+    expect(function () {
+        expect(__DIR__ . '/tmp/fileC.php')
+            ->not->toHaveContents('I\'m an empty file!');
+    })->toThrow(AssertionFailedError::class);
 });
